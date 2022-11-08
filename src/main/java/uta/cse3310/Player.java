893cda177e92f37cf6f61d7f78511de9ed161cfd
natasyakaramazov <kierraashford@gmail.com>
2022-11-08 15:15:31 -0600

    some screenshots

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -2,9 +2,17 @@
 
 package uta.cse3310; 
 
+
 public class Player{
     int amount = 200;
     int amountWon = 0; 
+     int PlayerId = 0;
+ String userName;
 
+     public Player(int id){
+     
+        PlayerId = id;
+        userName = "Not Entered";
+     }
     
 }
