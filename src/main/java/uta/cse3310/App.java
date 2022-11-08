e1556900833528db8c5cd466f763fc3e9ceed0b5
natasyakaramazov <kierraashford@gmail.com>
2022-11-07 21:08:32 -0600

    modified app to correct ports

--- src/main/java/uta/cse3310/App.java
+++ src/main/java/uta/cse3310/App.java
@@ -184,14 +184,14 @@ public class App extends WebSocketServer {
   public static void main(String[] args) {
 
     // Set up the http server
-    int port = 9080;
+    int port = 9081;
     HttpServer H = new HttpServer(port, "./html");
     H.start();
     System.out.println("http Server started on port:" + port);
 
     // create and start the websocket server
 
-    port = 9880;
+    port = 9881;
     App A = new App(port);
     A.start();
     System.out.println("websocket Server started on port: " + port);
