40e7102e4ff66e99083b123157b93fd4e4554925
Alex <Alexander.jones3@mavs.uta.edu>
2022-11-09 18:44:23 -0600

    Added a vector called hands to hold the cards

--- src/main/java/uta/cse3310/Dealer.java
+++ src/main/java/uta/cse3310/Dealer.java
@@ -2,10 +2,14 @@
 * Alexander Jones
 */
 package uta.cse3310;
+import java.util.Vector;
 
 public class Dealer
 {
 int amountWon = 0;
+
+Vector<Card> hand = new Vector<Card>(); 
+
 void logicCheck()
 {
     
