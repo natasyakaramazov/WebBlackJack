e195c486e5c9ac2fcb0166d0a413b05573610147
Alex <Alexander.jones3@mavs.uta.edu>
2022-11-15 19:50:21 -0600

    reorganized some elements

--- src/main/java/uta/cse3310/Dealer.java
+++ src/main/java/uta/cse3310/Dealer.java
@@ -6,12 +6,51 @@ import java.util.Vector;
 
 public class Dealer
 {
-int amountWon = 0;
+    int amountWon = 0; // Amount of money won by the dealer
+    int handamount;
+    int i = 0;
+    Vector<Card> hand = new Vector<Card>(); // What will hold the dealers cards
 
-Vector<Card> hand = new Vector<Card>(); 
 
-void logicCheck()
-{
+    Dealer() /*Created by Alexander Jones, Last Edited by Alexander Jones */
+    {
+        handsize();
+
+        while (handamount != 21)
+        {
+            logicCheck();
+        }
+
+
+    }
     
-}
+    /*
+     * void logicCheck() // this will be used to help the determine the best next move
+        {
+    
+        }
+     */
+
+
+    //while(Card.Cardvalue != 21 || Card.Cardvalue <= 21)
+
+
+
+    void handsize() /*Created by Alexander Jones, Last Edited by Alexander Jones */
+    {
+        
+        int i = 0;
+
+        for(i = 0; i < hand.size(); i++)
+        {
+            //handamount += hand[i].CardValue;
+        }
+
+    }
+
+    void logicCheck() /*Created by Alexander Jones, Last Edited by Alexander Jones */
+    {
+
+    }
+
 }
