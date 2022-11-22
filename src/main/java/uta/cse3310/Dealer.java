5e2f3372b30cce0ac1285bc026468d1cbc8503e2
Alex <Alexander.jones3@mavs.uta.edu>
2022-11-22 17:45:54 -0600

    added more functions and still working on logic check

--- src/main/java/uta/cse3310/Dealer.java
+++ src/main/java/uta/cse3310/Dealer.java
@@ -3,54 +3,87 @@
 */
 package uta.cse3310;
 import java.util.Vector;
+import java.util.Random;
+
 
 public class Dealer
 {
-    int amountWon = 0; // Amount of money won by the dealer
-    int handamount;
-    int i = 0;
-    Vector<Card> hand = new Vector<Card>(); // What will hold the dealers cards
-
+    int playerID = 0;                                   // Dealer playerID will always be 0
+    int amountWon = 0;                                 //  Amount of money won by the dealer
+    int dealerhandamount;                             //   The total amount of cards the dealer value will hold
+    int randnumber;                                  //    Integer value that will store random number
+    int i = 0;                                      //     Incrimentor
+    Event choice;                                  //      What choices the dealer will make
+    Vector<Card> dealerhand = new Vector<Card>(); //       What will hold the dealers cards
+    //Vector hand = new Vector();
 
     Dealer() /*Created by Alexander Jones, Last Edited by Alexander Jones */
-    {
-        handsize();
+    { 
+        Card tempCard = new Card();
+        dealerhand.add(tempCard);
+        
+        
 
-        while (handamount != 21)
+        if(dealerhand.size() == 0)
+        {
+            choice.hit();
+            if (tempCard.getCardValue() == CardValue.Ace)
+            {
+                // allow players to call for insurance
+            }
+        }
+        choice.hit();
+        dealerhandsize();
+
+        while (dealerhandamount != 21)
         {
             logicCheck();
         }
 
 
     }
-    
-    /*
-     * void logicCheck() // this will be used to help the determine the best next move
-        {
-    
-        }
-     */
-
-
-    //while(Card.Cardvalue != 21 || Card.Cardvalue <= 21)
 
 
 
-    void handsize() /*Created by Alexander Jones, Last Edited by Alexander Jones */
+    void getRandomNumber()  /* Created by Alexander Jones, Last Edited by Alexander Jones */
     {
-        
-        int i = 0;
+        // Generates a random number between 1 and 7 when called on, guarentees random number everytime
+        randnumber = (int) (Math.random() * (7 - 1)) + 1;
+    }
 
-        for(i = 0; i < hand.size(); i++)
+    void dealerhandsize() /* Created by Alexander Jones, Last Edited by Alexander Jones */
+    {
+        // gets the total sum of all the cards
+    
+
+        for(i = 0; i < dealerhand.size(); i++)
         {
-            //handamount += hand[i].CardValue;
+            //handamount += dealerhand[i].CardValue;
         }
 
     }
 
+
     void logicCheck() /*Created by Alexander Jones, Last Edited by Alexander Jones */
     {
+        // if(hand == 0) empty deck then draw first card
 
+        //if(dealerhand.get(0) == CardValue.Ace)  // if first card is an Ace, allow players the option to choose insurance
+        {
+            
+        }
+        if(dealerhandamount <= 17)                  // if value of the cards are less than 17, then have a rand event that will determine if they dealer will hit or double
+        {
+            if(randnumber < 5)                 // random number between 1-7, if you get a value less then 5, then stay, but if you get a value more than 5, chance it
+            {
+                choice.stand();
+            }
+            else
+            {
+                choice.hit();
+            }
+
+        }
     }
 
 }
